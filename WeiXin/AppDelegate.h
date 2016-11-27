0cfaf8b7de43c3569b53d813b979e577d8136acc
刘鹏 <lp_aideals@hotmail.com>
2016-11-27 15:17:36 +0800

    Initial Commit

---
+++ WeiXin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeiXin
+//
+//  Created by 鹏 刘 on 2016/11/27.
+//  Copyright © 2016年 鹏 刘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeiXin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeiXin
+//
+//  Created by 鹏 刘 on 2016/11/27.
+//  Copyright © 2016年 鹏 刘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
